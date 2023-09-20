4f524dfa6c1470ed43f52254c7f80b19f342b4cf
priana13 <herian.sap@gmail.com>
2023-09-20 20:45:58 +0700

    component dashboard

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,8 @@
 <x-guest-layout>
     <x-authentication-card>
         <x-slot name="logo">
-            <x-authentication-card-logo />
+            {{-- <x-authentication-card-logo /> --}}
+            <h2 class="text-2xl font-bold">LOGIN</h2>
         </x-slot>
 
         <x-validation-errors class="mb-4" />
---
+++ resources/views/components/dashboard/card-overview.blade.php
@@ -0,0 +1,62 @@
+<div class="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
+    <!-- Card Item Start -->
+    <div
+        class="rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
+       
+        <div class="mt-4 flex items-end justify-between">
+          <div>
+              <h4 class="text-title-md font-bold text-black dark:text-white">
+                Rp. 3.456K
+              </h4>
+              <span class="text-sm font-medium">Total Pemasukan/Hari</span>
+          </div>
+          
+        </div>
+    </div>
+    <!-- Card Item End -->
+    <!-- Card Item Start -->
+    <div
+        class="rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
+        
+        <div class="mt-4 flex items-end justify-between">
+          <div>
+              <h4 class="text-title-md font-bold text-black dark:text-white">
+                Rp. 45,2K
+              </h4>
+              <span class="text-sm font-medium">Total Pengeluaran/Hari</span>
+          </div>
+       
+        </div>
+    </div>
+    <!-- Card Item End -->
+    <!-- Card Item Start -->
+    <div
+        class="rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
+        
+        <div class="mt-4 flex items-end justify-between">
+          <div>
+              <h4 class="text-title-md font-bold text-black dark:text-white">
+                Rp. 2.450
+              </h4>
+              <span class="text-sm font-medium">Total Pemasukan/bulan</span>
+          </div>
+          
+        </div>
+    </div>
+    <!-- Card Item End -->
+    <!-- Card Item Start -->
+    <div
+        class="rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
+        
+        <div class="mt-4 flex items-end justify-between">
+          <div>
+              <h4 class="text-title-md font-bold text-black dark:text-white">
+                Rp. 3.456
+              </h4>
+              <span class="text-sm font-medium">Total Pengeluaran/bulan</span>
+          </div>
+         
+        </div>
+    </div>
+    <!-- Card Item End -->
+  </div>
